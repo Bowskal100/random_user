c9a3c7c2ad4715d2a7fe9a5876ed25e4e01159a4
Samuel Shobowale <samboske93@gmail.com>
2021-08-23 04:55:35 -0400

    testing revert configs

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ COPY ./requirements.txt /app/requirements.txt
 
 WORKDIR /app
 
-RUN pip3 install -r requirements.txt password='ilmwies' 
+RUN pip3 install -r requirements.txt
 
 COPY . /app
 
--- aws-git-secrets/pre-commit-mac.sh
+++ aws-git-secrets/pre-commit-mac.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-cd .. 
+#cd .. 
 # Note that this will be invoked by the git hook from the repo root, so cd .. isn't required
 
 # These only need to be run once per workstation but are included to try and ensure they are present
-./git-secrets --add-provider -- cat aws-git-secrets/aws-rules-linux-mac.txt
+./aws-git-secrets/git-secrets --add-provider -- cat aws-git-secrets/aws-rules-linux-mac.txt
 
 # Just scan the files changed in this commit
-./git-secrets --pre_commit_hook
+./aws-git-secrets/git-secrets --pre_commit_hook
